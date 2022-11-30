a4953a98f5d8442f11b6910f044fa3f9ba845267
thiagocoutinhodemelo <99850333+thiagocoutinhodemelo@users.noreply.github.com>
2022-11-29 22:35:47 -0300

    criação do projeto

---
+++ script.js
@@ -0,0 +1,13 @@
+const c = (el)=>document.querySelector(el);
+const cs = (el)=>document.querySelectorAll(el);
+
+
+pizzaJson.map((item, index)=>{
+    let pizzaItem = c('.models .pizza-item').cloneNode(true);
+
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+
+    c('.pizza-area').append( pizzaItem );
+});
