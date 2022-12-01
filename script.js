2fc2147234d5c337a27c517a7f045be2e06eaed0
thiagocoutinhodemelo <99850333+thiagocoutinhodemelo@users.noreply.github.com>
2022-12-01 11:05:49 -0300

    finalização js

--- script.js
+++ script.js
@@ -91,9 +91,18 @@ c('.pizzaInfo--addButton').addEventListener('click', ()=>{
     }
     updateCart();
     closeModal();
-})
+});
 
+c('.menu-openner').addEventListener('click', ()=>{
+    if(cart.length > 0){
+    c('aside').style.left = 0;
+    }
+});
+c('.menu-closer').addEventListener ('click', ()=>{
+    c('aside').style.left = '100vw';
+});
 function updateCart (){
+    c('.menu-openner span').innerHTML = cart.length;
     if(cart.length > 0){
         c('aside').classList.add('show');
         c('.cart').innerHTML = '';
@@ -148,6 +157,7 @@ function updateCart (){
         c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
 
     } else{
-
+        c('aside').classList.remove('show');
+        c('aside').style.left = '100vw';
     }
 }
